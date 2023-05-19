2720e4bb6e907155f36026b7570f1a2336d70ddb
francisdama1@gmail.com <francisdama1@gmail.com>
2023-05-19 16:27:41 +0100

    Users controller update v2

--- src/main/java/com/xclusive/political_web_app/User/UserController.java
+++ src/main/java/com/xclusive/political_web_app/User/UserController.java
@@ -4,10 +4,7 @@ import com.xclusive.political_web_app.Login.AllUserModel;
 import com.xclusive.political_web_app.Security.RegistrationDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +17,7 @@ public class UserController {
     private final UserService userService;
     private final RegistrationDetailsService userDetailService;
 
-    @GetMapping
+    @PostMapping
     public UsersResponsePojo getData(@RequestBody AllUserModel model){
 
         List<AppUser> allAppUsers = new ArrayList<>();
